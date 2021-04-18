0b78b19239a7d08e3475af835fb7f980c8a86021
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-19 00:33:08 +0300

    Tekrarlı kodu önlemek için data yüklemesi başka sınıfa alındı

---
+++ BuraksRecordShopHRM.App/Pages/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using BuraksRecordShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace BuraksRecordShopHRM.App.Pages
+{
+    public static class DataLoader
+    {
+        public static IEnumerable<JobCategory> LoadJobs() => new List<JobCategory>
+            {
+                new JobCategory{JobCategoryId=1,Name="Research Lab" },
+                new JobCategory{JobCategoryId=2,Name="Finance" },
+                new JobCategory{JobCategoryId=3,Name="IT" },
+                new JobCategory{JobCategoryId=4,Name="Shop Assistant" },
+                new JobCategory{JobCategoryId=5,Name="Store Staff" },
+            };
+
+        public static IEnumerable<Country> LoadCountries() => new List<Country>
+            {
+                new Country{CountryId=1,Name="Germany"},
+                new Country{CountryId=2,Name="England"},
+                new Country{CountryId=3,Name="Turkey"},
+                new Country{CountryId=4,Name="Portugal"},
+                new Country{CountryId=5,Name="Spain"},
+            };
+
+        public static IEnumerable<Employee> LoadEmployees() => new List<Employee>
+            {
+                new Employee{ EmployeeId=1,FirstName="Junger",LastName="Klöp", BirthDate=new DateTime(1967,4,12), CountryId=1, JobCategoryId=1},
+                new Employee{ EmployeeId=2,FirstName="Aleksa",LastName="Bröger", BirthDate=new DateTime(1974,1,11), CountryId=1, JobCategoryId=3},
+                new Employee{ EmployeeId=3,FirstName="Burak",LastName="Selim", BirthDate=new DateTime(1976,4,12), CountryId=3, JobCategoryId=2},
+                new Employee{ EmployeeId=4,FirstName="Camelia Oscar De La",LastName="Fuante Garcia Velazques", BirthDate=new DateTime(1985,3,8), CountryId=5, JobCategoryId=4}
+            };
+    }
+}
