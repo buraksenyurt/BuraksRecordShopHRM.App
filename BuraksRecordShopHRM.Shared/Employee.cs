45856fd22ab3d245a9ded6ca4dfec7f4d4b33dfa
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-18 23:38:55 +0300

    Birkaç entity eklendi

---
+++ BuraksRecordShopHRM.Shared/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BuraksRecordShopHRM.Shared
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public int JobCategoryId { get; set; }
+        public JobCategory JobCategory { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ BuraksRecordShopHRM.Shared/JobCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace BuraksRecordShopHRM.Shared
+{
+    public class JobCategory
+    {
+        public int JobCategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
