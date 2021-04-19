1d1001f498a47844b94a9ed03da3a868f9407ad0
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-20 00:21:21 +0300

    Country Data Service eklendi

---
+++ BuraksRecordShopHRM.App/Services/CountryDataService.cs
@@ -0,0 +1,36 @@
+﻿using BuraksRecordShopHRM.Shared;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BuraksRecordShopHRM.App.Services
+{
+    public class CountryDataService
+        : ICountryDataService
+    {
+        public readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        public CountryDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<IEnumerable<Country>> GetAll()
+        {
+            var response = await _httpClient.GetStreamAsync("api/country");
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Country>>(
+                response,
+                options);
+        }
+
+        public async Task<Country> GetById(int countryId)
+        {
+            var response = await _httpClient.GetStreamAsync($"api/country/{countryId}");
+            return await JsonSerializer.DeserializeAsync<Country>(
+                response,
+                options);
+        }
+    }
+}
---
+++ BuraksRecordShopHRM.App/Services/ICountryDataService.cs
@@ -0,0 +1,12 @@
+﻿using BuraksRecordShopHRM.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BuraksRecordShopHRM.App.Services
+{
+    public interface ICountryDataService
+    {
+        Task<IEnumerable<Country>> GetAll();
+        Task<Country> GetById(int countryId);
+    }
+}
