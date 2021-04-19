647ef59a3d7409e0ac5c4b818238ac1452de8f76
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-20 00:21:35 +0300

    JobCategory Data Service eklendi

---
+++ BuraksRecordShopHRM.App/Services/IJobCategoryDataService.cs
@@ -0,0 +1,12 @@
+﻿using BuraksRecordShopHRM.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BuraksRecordShopHRM.App.Services
+{
+    public interface IJobCategoryDataService
+    {
+        Task<IEnumerable<JobCategory>> GetAll();
+        Task<JobCategory> GetById(int jobCategoryId);
+    }
+}
---
+++ BuraksRecordShopHRM.App/Services/JobCategoryDataService.cs
@@ -0,0 +1,34 @@
+﻿using BuraksRecordShopHRM.Shared;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BuraksRecordShopHRM.App.Services
+{
+    public class JobCategoryDataService
+        : IJobCategoryDataService
+    {
+        public readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        public JobCategoryDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<JobCategory>> GetAll()
+        {
+            var response = await _httpClient.GetStreamAsync("api/jobcategory");
+            return await JsonSerializer.DeserializeAsync<IEnumerable<JobCategory>>(
+                response,
+                options);
+        }
+
+        public async Task<JobCategory> GetById(int jobCategoryId)
+        {
+            var response = await _httpClient.GetStreamAsync($"api/jobcategory/{jobCategoryId}");
+            return await JsonSerializer.DeserializeAsync<JobCategory>(
+                response,
+                options);
+        }
+    }
+}
