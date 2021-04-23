fa30d09ffad209ce7e99aca35c4ce02b65a7b9f5
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-23 18:49:06 +0300

    Map bileşeni eklendi

---
+++ BuraksRecordShopHRM.Components/Map/Marker.cs
@@ -0,0 +1,10 @@
+﻿namespace BuraksRecordShopHRM.Components.Map
+{
+    public class Marker
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Description { get; set; }
+        public bool ShowPopup { get; set; }
+    }
+}
