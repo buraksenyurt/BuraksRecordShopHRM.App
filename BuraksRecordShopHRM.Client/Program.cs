72a28de07e9a32f9ade38753a29a4ae62efbc665
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-24 00:53:48 +0300

    App tarafındaki Program.cs'e ihtiyaç kalmadı. Bu Client projesinde gerekli.

--- BuraksRecordShopHRM.Client/Program.cs
+++ BuraksRecordShopHRM.Client/Program.cs
@@ -1,7 +1,7 @@
+﻿using BuraksRecordShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BuraksRecordShopHRM.Client
@@ -11,10 +11,10 @@ namespace BuraksRecordShopHRM.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.RootComponents.Add<BuraksRecordShopHRM.App.App>("app");
+            builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
+            builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
+            builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
             await builder.Build().RunAsync();
         }
     }
