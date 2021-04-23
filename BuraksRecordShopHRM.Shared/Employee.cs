d5215d2b259e703c1916914ff8c2368dbd1a86cd
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-23 16:18:34 +0300

    Alan bazlı doğrulama özellikleri eklendi

--- BuraksRecordShopHRM.Shared/Employee.cs
+++ BuraksRecordShopHRM.Shared/Employee.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BuraksRecordShopHRM.Shared
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Required]
+        [StringLength(50,ErrorMessage = "But the long name!")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "But the long surname!")]
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public int JobCategoryId { get; set; }
         public JobCategory JobCategory { get; set; }
