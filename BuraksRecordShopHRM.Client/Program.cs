458865523bb99790401d61cae5c6c848ae1dccd0
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-24 01:00:58 +0300

    Servis register işlemleri ve build hataları.

--- BuraksRecordShopHRM.Client/Program.cs
+++ BuraksRecordShopHRM.Client/Program.cs
@@ -11,7 +11,7 @@ namespace BuraksRecordShopHRM.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<BuraksRecordShopHRM.App.App>("app");
+            builder.RootComponents.Add<App.App>("app");
             builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
             builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
             builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri("https://localhost:44364"));
