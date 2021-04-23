c736800de0f9a69d9cd2d117e88dc7f7ac41377f
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-23 17:19:28 +0300

    Hızlı personel ekleme işinde kullanılacak dialog tipinden bileşenin geliştirilmesine başlandı.

---
+++ BuraksRecordShopHRM.App/Components/QuickCreateEmployeeDialog.cs
@@ -0,0 +1,15 @@
+﻿using BuraksRecordShopHRM.App.Services;
+using BuraksRecordShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BuraksRecordShopHRM.App.Components
+{
+    public partial class QuickCreateEmployeeDialog
+    {
+        public Employee Employee { get; set; } = new Employee { JobCategoryId = 1, CountryId = 1, BirthDate = DateTime.Now.AddYears(-20), JoinedDate = DateTime.Now };
+        [Inject]
+        public EmployeeDataService EmployeeDataService{ get; set; }
+        public bool ShowDialog { get; set; }
+    }
+}
