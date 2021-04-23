16e66db677230d16033ce33cdcf6fa78524a2df9
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-23 23:25:25 +0300

    Harita için ilk girilen çalışanlara koordinat bilgisi eklendi

--- BuraksRecordShopHRM.Api/Models/AppDbContext.cs
+++ BuraksRecordShopHRM.Api/Models/AppDbContext.cs
@@ -33,10 +33,10 @@ namespace BuraksRecordShopHRM.Api.Models
             modelBuilder.Entity<JobCategory>().HasData(new JobCategory() { JobCategoryId = 4, Name = "Shop Assistant" });
             modelBuilder.Entity<JobCategory>().HasData(new JobCategory() { JobCategoryId = 5, Name = "Store Staff" });
 
-            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 1, FirstName = "Junger", LastName = "Klöp", BirthDate = new DateTime(1967, 4, 12), JoinedDate = new DateTime(1990, 1, 8), Email = "junger.clup@buraksrecords.com", CountryId = 1, JobCategoryId = 1 });
-            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 2, FirstName = "Aleksa", LastName = "Bröger", BirthDate = new DateTime(1974, 1, 11), JoinedDate = new DateTime(2000, 1, 8), Email = "aleksa.broger@buraksrecords.com", CountryId = 1, JobCategoryId = 3 });
-            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 3, FirstName = "Burak", LastName = "Selim", BirthDate = new DateTime(1976, 4, 12), JoinedDate = new DateTime(2001, 12, 10), Email = "burak.selim@buraksrecords.com", CountryId = 3, JobCategoryId = 2 });
-            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 4, FirstName = "Camelia Oscar De La", LastName = "Fuante Garcia Velazques", BirthDate = new DateTime(1985, 3, 8), JoinedDate = new DateTime(2010, 9, 9), Email = "camelia.veazques@buraksrecords.com", CountryId = 5, JobCategoryId = 4 });
+            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 1, FirstName = "Junger", LastName = "Klöp", BirthDate = new DateTime(1967, 4, 12), JoinedDate = new DateTime(1990, 1, 8), Email = "junger.clup@buraksrecords.com", CountryId = 1, JobCategoryId = 1,Latitude= 52.52,Longitude= 13.405 });
+            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 2, FirstName = "Aleksa", LastName = "Bröger", BirthDate = new DateTime(1974, 1, 11), JoinedDate = new DateTime(2000, 1, 8), Email = "aleksa.broger@buraksrecords.com", CountryId = 1, JobCategoryId = 3, Latitude = 52.52, Longitude = 13.405 });
+            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 3, FirstName = "Burak", LastName = "Selim", BirthDate = new DateTime(1976, 4, 12), JoinedDate = new DateTime(2001, 12, 10), Email = "burak.selim@buraksrecords.com", CountryId = 3, JobCategoryId = 2, Latitude = 32.2480, Longitude = 112.9161 });
+            modelBuilder.Entity<Employee>().HasData(new Employee { EmployeeId = 4, FirstName = "Camelia Oscar De La", LastName = "Fuante Garcia Velazques", BirthDate = new DateTime(1985, 3, 8), JoinedDate = new DateTime(2010, 9, 9), Email = "camelia.veazques@buraksrecords.com", CountryId = 5, JobCategoryId = 4, Latitude = 66.5667, Longitude = 0.0000 });
         }
     }
 }
